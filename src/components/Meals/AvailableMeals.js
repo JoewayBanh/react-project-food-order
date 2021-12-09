6222f4d487065031a507a039db5be207dd2999de
Joeway Banh <joewaybanh@hotmail.com>
2021-12-09 16:17:39 +0100

    added error handling when trying to fetch

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,12 +6,18 @@ import { useState } from "react";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-food-order-project-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
       const responseData = await response.json();
       const loadedMeals = [];
@@ -26,11 +32,30 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
+  if (isLoading) {
+    return (
+      <section>
+        <h1 className={classes.mealsLoading}>Loading...</h1>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section>
+        <h1 className={classes.mealsError}>{httpError}</h1>
+      </section>
+    );
+  }
   const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
